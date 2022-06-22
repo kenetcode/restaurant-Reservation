677801ded85e7a20f2da34d8fcb254e3556b4d00
kenetcode <k2ken2424@gmail.com>
2022-06-22 00:37:07 -0600

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace restaurant10TablesReservationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
